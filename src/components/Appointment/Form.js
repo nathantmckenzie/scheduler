bfd39fb2cb29600eead791eb85d4400b2d112d9f
Nathan McKenzie <nathantaylormckenzie@hotmail.com>
2020-12-16 12:59:11 -0800

    Test coverage

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -6,6 +6,7 @@ import Button from 'components/Button';
 export default function Form(props) {
   const [name, setName] = useState(props.name || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || "");
+  const [error, setError] = useState("");
 
   const reset = () => {
     setName("");
@@ -17,6 +18,14 @@ export default function Form(props) {
     props.onCancel();
   }
 
+  function validate() {
+    if (name === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+    setError("");
+    props.onSave(name, interviewer);
+  }
     
   return (
     <main className="appointment__card appointment__card--create">
@@ -34,6 +43,7 @@ export default function Form(props) {
          data-testid="student-name-input"
       />
       </form>
+      <section className="appointment__validation">{error}</section>
       <InterviewerList 
          interviewers={props.interviewers} 
          value={interviewer} 
